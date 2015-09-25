b24c547883613b7c1d123f5ad8394c8994c245a0
Mickey Barboi <damouse007@gmail.com>
2015-09-24 21:45:50 -0500

    pods and basics

---
+++ swiftdemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  swiftdemo-Bridging-Header.h
+//  swiftdemo
+//
+//  Created by Mickey Barboi on 9/24/15.
+//  Copyright © 2015 paradrop. All rights reserved.
+//
+
+#ifndef swiftdemo_Bridging_Header_h
+#define swiftdemo_Bridging_Header_h
+
+#import "MDWamp.h"
+
+#endif /* swiftdemo_Bridging_Header_h */
--- swiftdemo/ViewController.swift
+++ swiftdemo/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let URL = "ws://ubuntu@ec2-52-26-83-61.us-west-2.compute.amazonaws.com:8000/ws"
+//ws://localhost:8080/ws
+
+class ViewController: UIViewController, MDWampClientDelegate {
+    var socket: MDWampTransportWebSocket?
+    var session: MDWamp?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        MDWampTransportWebSocket *websocket = [[MDWampTransportWebSocket alloc] initWithServer:[NSURL URLWithString:@"ws://localhost:8080/ws"] protocolVersions:@[kMDWampProtocolWamp2msgpack, kMDWampProtocolWamp2json]];
+//        
+//        _wamp = [[MDWamp alloc] initWithTransport:websocket realm:@"realm1" delegate:self];
+        
+        print("Attempting to connect...")
+        socket = MDWampTransportWebSocket(server:NSURL(string: URL), protocolVersions:[kMDWampProtocolWamp2msgpack, kMDWampProtocolWamp2json])
+        session = MDWamp(transport: socket, realm: "pd.damouse", delegate: self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func mdwamp(wamp: MDWamp!, sessionEstablished info: [NSObject : AnyObject]!) {
+        print("Session Established!")
+    }
+    
+    func mdwamp(wamp: MDWamp!, closedSession code: Int, reason: String!, details: [NSObject : AnyObject]!) {
+        print("Session Closed!")
+    }
 
 }
 
