9e3885ccfd91b0a572ca46cf42e29b1e3c849f0e
Mickey Barboi <damouse007@gmail.com>
2015-09-24 21:48:28 -0500

    connection up

--- swiftdemo/ViewController.swift
+++ swiftdemo/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 let URL = "ws://ubuntu@ec2-52-26-83-61.us-west-2.compute.amazonaws.com:8000/ws"
-//ws://localhost:8080/ws
+//let URL = "ws://localhost:8000/ws"
+
 
 class ViewController: UIViewController, MDWampClientDelegate {
     var socket: MDWampTransportWebSocket?
@@ -26,6 +27,7 @@ class ViewController: UIViewController, MDWampClientDelegate {
         print("Attempting to connect...")
         socket = MDWampTransportWebSocket(server:NSURL(string: URL), protocolVersions:[kMDWampProtocolWamp2msgpack, kMDWampProtocolWamp2json])
         session = MDWamp(transport: socket, realm: "pd.damouse", delegate: self)
+        session?.connect()
     }
 
     override func didReceiveMemoryWarning() {
